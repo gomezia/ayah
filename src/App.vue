99fc4f3190da75f3731ac21294e286b11308482d
Mustapha MEZIANE <mustapha@Mac-mini-de-Ausy.local>
2017-06-15 12:03:57 +0200

    add content form

--- src/App.vue
+++ src/App.vue
@@ -16,56 +16,56 @@
   			</div>
 
   	    	<div class="sidebar-wrapper">
-  				<ul class="nav">
-  	                <li>
-  	                    <a href="dashboard.html">
-  	                        <i class="material-icons">dashboard</i>
-  	                        <p>Dashboard</p>
-  	                    </a>
-  	                </li>
-  	                <li class="active">
-  	                    <a href="user.html">
-  	                        <i class="material-icons">person</i>
-  	                        <p>User Profile</p>
-  	                    </a>
-  	                </li>
-  	                <li>
-  	                    <a href="table.html">
-  	                        <i class="material-icons">content_paste</i>
-  	                        <p>Table List</p>
-  	                    </a>
-  	                </li>
-  	                <li>
-  	                    <a href="typography.html">
-  	                        <i class="material-icons">library_books</i>
-  	                        <p>Typography</p>
-  	                    </a>
-  	                </li>
-  	                <li>
-  	                    <a href="icons.html">
-  	                        <i class="material-icons">bubble_chart</i>
-  	                        <p>Icons</p>
-  	                    </a>
-  	                </li>
-  	                <li>
-  	                    <a href="maps.html">
-  	                        <i class="material-icons">location_on</i>
-  	                        <p>Maps</p>
-  	                    </a>
-  	                </li>
-  	                <li>
-  	                    <a href="notifications.html">
-  	                        <i class="material-icons text-gray">notifications</i>
-  	                        <p>Notifications</p>
-  	                    </a>
-  	                </li>
-  					<li class="active-pro">
-                          <a href="upgrade.html">
-  	                        <i class="material-icons">unarchive</i>
-  	                        <p>Upgrade to PRO</p>
-  	                    </a>
-                      </li>
-  	            </ul>
+    				<ul class="nav">
+              <li>
+                  <a href="dashboard.html">
+                      <i class="material-icons">dashboard</i>
+                      <p>Dashboard</p>
+                  </a>
+              </li>
+              <li class="active">
+                  <a href="user.html">
+                      <i class="material-icons">person</i>
+                      <p>User Profile</p>
+                  </a>
+              </li>
+              <li>
+                  <a href="table.html">
+                      <i class="material-icons">content_paste</i>
+                      <p>Table List</p>
+                  </a>
+              </li>
+              <li>
+                  <a href="typography.html">
+                      <i class="material-icons">library_books</i>
+                      <p>Typography</p>
+                  </a>
+              </li>
+              <li>
+                  <a href="icons.html">
+                      <i class="material-icons">bubble_chart</i>
+                      <p>Icons</p>
+                  </a>
+              </li>
+              <li>
+                  <a href="maps.html">
+                      <i class="material-icons">location_on</i>
+                      <p>Maps</p>
+                  </a>
+              </li>
+              <li>
+                  <a href="notifications.html">
+                      <i class="material-icons text-gray">notifications</i>
+                      <p>Notifications</p>
+                  </a>
+              </li>
+  		       <li class="active-pro">
+                <a href="upgrade.html">
+                  <i class="material-icons">unarchive</i>
+                  <p>Upgrade to PRO</p>
+                </a>
+              </li>
+    	       </ul>
   	    	</div>
   	    </div>
 
@@ -126,117 +126,40 @@
 
   	        <div class="content">
   	            <div class="container-fluid">
+                  
+
+
   	                <div class="row">
   	                    <div class="col-md-8">
   	                        <div class="card">
-  	                            <div class="card-header" data-background-color="purple">
-  	                                <h4 class="title">Edit Profile</h4>
-  									<p class="category">Complete your profile</p>
-  	                            </div>
   	                            <div class="card-content">
   	                                <form>
   	                                    <div class="row">
-  	                                        <div class="col-md-5">
-  												<div class="form-group label-floating">
-  													<label class="control-label">Company (disabled)</label>
-  													<input type="text" class="form-control" disabled>
-  												</div>
-  	                                        </div>
-  	                                        <div class="col-md-3">
-  												<div class="form-group label-floating">
-  													<label class="control-label">Username</label>
-  													<input type="text" class="form-control" >
-  												</div>
-  	                                        </div>
-  	                                        <div class="col-md-4">
-  												<div class="form-group label-floating">
-  													<label class="control-label">Email address</label>
-  													<input type="email" class="form-control" >
-  												</div>
-  	                                        </div>
-  	                                    </div>
-
-  	                                    <div class="row">
-  	                                        <div class="col-md-6">
-  												<div class="form-group label-floating">
-  													<label class="control-label">Fist Name</label>
-  													<input type="text" class="form-control" >
-  												</div>
-  	                                        </div>
-  	                                        <div class="col-md-6">
-  												<div class="form-group label-floating">
-  													<label class="control-label">Last Name</label>
-  													<input type="text" class="form-control" >
-  												</div>
-  	                                        </div>
-  	                                    </div>
-
-  	                                    <div class="row">
   	                                        <div class="col-md-12">
-  												<div class="form-group label-floating">
-  													<label class="control-label">Adress</label>
-  													<input type="text" class="form-control" >
-  												</div>
+                      												<div class="form-group label-floating">
+                      													<label class="control-label">Title</label>
+                      													<input type="text" class="form-control" >
+                      												</div>
   	                                        </div>
   	                                    </div>
-
-  	                                    <div class="row">
-  	                                        <div class="col-md-4">
-  												<div class="form-group label-floating">
-  													<label class="control-label">City</label>
-  													<input type="text" class="form-control" >
-  												</div>
-  	                                        </div>
-  	                                        <div class="col-md-4">
-  												<div class="form-group label-floating">
-  													<label class="control-label">Country</label>
-  													<input type="text" class="form-control" >
-  												</div>
-  	                                        </div>
-  	                                        <div class="col-md-4">
-  												<div class="form-group label-floating">
-  													<label class="control-label">Postal Code</label>
-  													<input type="text" class="form-control" >
-  												</div>
-  	                                        </div>
-  	                                    </div>
-
   	                                    <div class="row">
   	                                        <div class="col-md-12">
   	                                            <div class="form-group">
-  	                                                <label>About Me</label>
-  													<div class="form-group label-floating">
-  									    				<label class="control-label"> Lamborghini Mercy, Your chick she so thirsty, I'm in that two seat Lambo.</label>
-  								    					<textarea class="form-control" rows="5"></textarea>
-  		                        					</div>
+  	                                                <label>Body</label>
+                          													<div class="form-group label-floating">
+                          								    					<div id="summernote"></div>
+                                                        <!--<div id="summernote">Hello Summernote</div> -->
+  		                        					             </div>
   	                                            </div>
   	                                        </div>
   	                                    </div>
 
-  	                                    <button type="submit" class="btn btn-primary pull-right">Update Profile</button>
+  	                                    <button type="submit" class="btn btn-primary pull-right">Save</button>
   	                                    <div class="clearfix"></div>
   	                                </form>
   	                            </div>
   	                        </div>
   	                    </div>
-  						<div class="col-md-4">
-      						<div class="card card-profile">
-      							<div class="card-avatar">
-      								<a href="#pablo">
-      									<img class="img" src="/src/assets/img/faces/marc.jpg" />
-      								</a>
-      							</div>
-
-      							<div class="content">
-      								<h6 class="category text-gray">CEO / Co-Founder</h6>
-      								<h4 class="card-title">Alec Thompson</h4>
-      								<p class="card-content">
-      									Don't be scared of the truth because we need to restart the human foundation in truth And I love you like Kanye loves Kanye I love Rick Owens’ bed design but the back is...
-      								</p>
-      								<a href="#pablo" class="btn btn-primary btn-round">Follow</a>
-      							</div>
-      						</div>
-  		    			</div>
   	                </div>
   	            </div>
   	        </div>
@@ -281,6 +204,11 @@
 </template>
 
 <script>
+
+$(document).ready(function(){
+  $('#summernote').summernote();
+});
+
 export default {
   name: 'app',
   data () {
